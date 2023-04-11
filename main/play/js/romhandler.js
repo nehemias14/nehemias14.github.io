1899c58781e44e4664377d59a32bd237c7cfc945
Logan <thecrazykid2121@gmail.com>
2023-04-11 14:37:45 -0400

    Another refactor + path update

---
+++ main/play/js/romhandler.js
@@ -0,0 +1,27 @@
+/*
+let providedGame = textToFilter;
+        let console = sendConsoleInfo();
+        let romLocation = localStorage.getItem('romLocation');
+        let ext;
+        if (console == "gba") {
+            ext = ".gba";
+        } else if (console == "gb") {
+            ext = ".7z";
+        } else if (console == "nds") {
+            ext = ".zip";
+        } else if (console == "nes") {
+            ext = ".nes.zip";
+        } else if (console == "snes") {
+            ext = ".zip";
+        } else if (console == "n64") {
+            ext = ".7z";
+        } else if (console == "gbc") {
+            ext = ".7z";
+        } else if (console == "segamd") {
+            ext = ".7z";
+        } else {
+            ext = ".zip";
+        }
+        let newLink = romLocation + console + '-alt/' + providedGame + ext;
+        return newLink;
+    */
--- main/scripts/elements.js
+++ main/scripts/elements.js
@@ -51,29 +51,8 @@ class RomList extends HTMLElement {
 
     function sendGameInfo() {
         let providedGame = textToFilter;
-        let console = sendConsoleInfo();
-        let romLocation = localStorage.getItem('romLocation');
-        let ext;
-        if (console == "gba") {
-            ext = ".gba";
-        } else if (console == "gb") {
-            ext = ".7z";
-        } else if (console == "nds") {
-            ext = ".zip";
-        } else if (console == "nes") {
-            ext = ".nes.zip";
-        } else if (console == "snes") {
-            ext = ".zip";
-        } else if (console == "n64") {
-            ext = ".7z";
-        } else if (console == "gbc") {
-            ext = ".7z";
-        } else if (console == "segamd") {
-            ext = ".7z";
-        } else {
-            ext = ".zip";
-        }
-        let newLink = romLocation + console + '-alt/' + providedGame + ext;
+        let consoleType = sendConsoleInfo();
+        let newLink = '../../play/index.html?console=' + consoleType + '&game=' + providedGame;
         return newLink;
     }
   }
