a481c50735f10d359dd962993a388ad5aca4a25b
Logan <thecrazykid2121@gmail.com>
2023-04-13 14:08:12 -0400

    Filters + other search additions
    Added filters
    Query will now auto-activate filter w/ that console
    It can be disabled after that
    Also toyed with strength of search

--- main/search/handler/search.js
+++ main/search/handler/search.js
@@ -4,13 +4,19 @@
 var urlParams = new URLSearchParams(window.location.search);
 var consoleFilter = urlParams.get("console");
 
+if (consoleFilter) {
+let filterQuery = document.getElementById(consoleFilter);
+filterQuery.classList.add("active");
+filterQuery.classList.add(consoleFilter);
+}
 // Filter games by console
 var filteredGames = games.filter(function (game) {
   game.name = game.name
     .replace(/(\([^\)]*\)|\.[^.]+|_=)+$/g, "")
-    .replace(/_/g, " "); // Remove all underscores from the game name
-  return !consoleFilter || game.console === consoleFilter;
+  return games;
 });
+
+
 
 // Create index and add documents to it
 var index = lunr(function () {
@@ -32,64 +38,93 @@ var index = lunr(function () {
   }, this);
 });
 
+
+let filters = document.querySelectorAll(".filter");
+filters.forEach(function (filter) {
+    filter.addEventListener("click", function () {
+        filter.classList.toggle("active");
+        let id = filter.id;
+        filter.classList.toggle(id);
+        handleSearchInput();
+    });
+});
+
+
 var searchInput = document.querySelector("#search-input");
 searchInput.addEventListener("input", handleSearchInput);
 
 function handleSearchInput() {
-  var query = searchInput.value.toLowerCase() + "*"; // Convert query to lowercase and add wildcard
-  var searchResults = document.querySelector("#search-results");
-
-  if (searchInput.value === "") {
+    var query = searchInput.value.toLowerCase() + "*"; // Convert query to lowercase and add wildcard
+    var searchResults = document.querySelector("#search-results");
+  
+    if (searchInput.value === "") {
+      searchResults.innerHTML = "";
+      return;
+    }
+  
+    // Clear existing search results
     searchResults.innerHTML = "";
-    return;
-  }
-
-  // Clear existing search results
-  searchResults.innerHTML = "";
-
-  // Perform a case-insensitive and partial search on the index
-  var results = index.query(function (q) {
-    q.term(query, {
-      boost: 10,
-      wildcard: lunr.Query.wildcard.TRAILING | lunr.Query.wildcard.LEADING,
-      usePipeline: true,
-    });
-    q.term(query, {
-      boost: 10,
-      wildcard: lunr.Query.wildcard.TRAILING | lunr.Query.wildcard.LEADING,
-      usePipeline: true,
-    });
-  });
-
-  // Loop through search results and show
-  if (results.length > 0) {
-    var numResults = Math.min(results.length, 50); // Limit to 50 results
-    for (var i = 0; i < numResults; i++) {
-      var result = results[i];
-      var game = filteredGames.find(function (g) {
-        return g.name === result.ref;
-      });
-      var li = document.createElement("li");
-      if (game) {
-        li.textContent = game.name; //.replace(/_/g, ' '); // Remove all underscores from the game name
-      } else {
-        li.textContent = "No Games Found";
+  
+    // Filter by console if console filters are active
+    var consoleFilters = document.querySelectorAll(".filter.active");
+    var filteredGamesByConsole = filteredGames.filter(function (game) {
+      if (consoleFilters.length === 0) {
+        return true;
       }
-      let consoleType = game.console;
-      let providedGame = game.name;
-      let newLink =
-        "../play/index.html?console=" + consoleType + "&game=" + providedGame;
-      li.onclick = function () {
-        window.location.href = newLink;
-      };
-      li.classList.add("rom-title");
+      var hasMatchingConsole = false;
+      for (var i = 0; i < consoleFilters.length; i++) {
+        if (game.console === consoleFilters[i].id) {
+          hasMatchingConsole = true;
+          break;
+        }
+      }
+      return hasMatchingConsole;
+    });
+  
+    // Perform a case-insensitive and partial search on the index
+    var results = index.query(function (q) {
+      q.term(query, {
+        boost: 10,
+        wildcard: lunr.Query.wildcard.TRAILING | lunr.Query.wildcard.LEADING,
+        usePipeline: true,
+      });
+      q.term(query, {
+        boost: 10,
+        wildcard: lunr.Query.wildcard.TRAILING | lunr.Query.wildcard.LEADING,
+        usePipeline: true,
+      });
+    });
+  
+    // Loop through search results and show
+    if (results.length > 0) {
+      var numResults = Math.min(results.length, 50); // Limit to 50 results
+      for (var i = 0; i < numResults; i++) {
+        var result = results[i];
+        var game = filteredGamesByConsole.find(function (g) {
+          return g.name === result.ref;
+        });
+        var li = document.createElement("li");
+        if (game) {
+          li.textContent = game.name.replace(/_/g, ' '); // Remove all underscores from the game name
+          li.classList.add(game.console);
+        } else {
+          li.textContent = "No Games Found";
+        }
+        let consoleType = game.console;
+        let providedGame = game.name;
+        let newLink =
+          "../play/index.html?console=" + consoleType + "&game=" + providedGame;
+        li.onclick = function () {
+          window.location.href = newLink;
+        };
+        li.classList.add("rom-title");
+        searchResults.appendChild(li);
+      }
+    } else {
+      // Otherwise nothing found
+      var li = document.createElement("li");
+      li.textContent = "No results found";
+      li.style.textAlign = "center";
       searchResults.appendChild(li);
     }
-  } else {
-    // Otherwise nothing found
-    var li = document.createElement("li");
-    li.textContent = "No results found";
-    li.style.textAlign = "center";
-    searchResults.appendChild(li);
   }
-}
