28133c26e4ef5373e5f5f526d75e3fc328f05d46
Logan <thecrazykid2121@gmail.com>
2023-04-13 08:22:44 -0400

    Fix home? + Wording

--- main/scripts/nav.js
+++ main/scripts/nav.js
@@ -7,17 +7,28 @@ const home = document.getElementById("homeBtn");
 const back = document.getElementById("backBtn");
 const options = document.getElementById("options");
 
-var homeLink = "/";
+function findHome() {
+  let currentPageUrl = window.location.pathname;
+  let pathArray = currentPageUrl.split("/");
+  let relativePath = "";
+  for (let i = 1; i < pathArray.length - 2; i++) {
+    relativePath += "../";
+  }
+  console.log(relativePath)
+  return relativePath;
+}
+
+var homeLink = findHome();
 var optionsLink = "/options";
 
-home.addEventListener("click", function() {
-    window.location = homeLink;
+home.addEventListener("click", function () {
+  window.location = homeLink;
 });
 
-back.addEventListener("click", function() {
-    history.back();
+back.addEventListener("click", function () {
+  history.back();
 });
 
-options.addEventListener("click", function() {
-    window.location = optionsLink;
+options.addEventListener("click", function () {
+  window.location = optionsLink;
 });
